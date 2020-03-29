9d3ae5f54f4575ac5452a8c66a020f7cca4dd68a
mike <michael.schneeb@gmail.com>
2020-03-29 15:40:01 +0200

    Release v.0.0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='dataclass_abc-abc',
-    version='0.0.2',
+    version='0.0.3',
     description='Library that lets you define abstract properties for dataclasses.',
     long_description=long_description,
     long_description_content_type='text/markdown',
