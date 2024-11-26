32e4496724be085d7cc803fde32d05bbfd9224f3
Michael Schneeberger <michael.schneeberger@fhnw.ch>
2024-11-26 15:58:09 +0100

    Simplify dataclassabc implementation for compatibility with Python 3.13 dataclasses and increase library version.

--- dataclassabc/__init__.py
+++ dataclassabc/__init__.py
@@ -2,14 +2,10 @@ import sys
 from dataclasses import (
     _FIELD,  # type: ignore
     _FIELD_INITVAR,  # type: ignore
-    _POST_INIT_NAME,  # type: ignore
     MISSING as _MISSING,
-    _fields_in_init_order,  # type: ignore
-    _init_fn,  # type: ignore
-    _process_class,  # type: ignore
-    _set_new_attribute,  # type: ignore
     Field as _Field,
     field as _field,
+    dataclass as _dataclass,
 )
 from typing import Generic, TypeVar, Type, Callable
 
@@ -18,7 +14,6 @@ from typing_extensions import (
     overload as _overload,
     dataclass_transform as _dataclass_transform,
 )
-
 
 __all__ = ["dataclassabc"]
 _T = TypeVar("_T")
@@ -185,74 +180,43 @@ def dataclassabc(
         name: str  # Implements the abstract property 'name'
     ```
     """
+
     def wrap(cls):
-        try:
-            cls = _process_class(
-                cls,
-                init=False,
-                repr=repr,
-                eq=eq,
-                order=order,
-                unsafe_hash=unsafe_hash,
-                frozen=frozen,
-                match_args=match_args,
-                kw_only=kw_only,
-                slots=slots,
-                weakref_slot=weakref_slot,
-            )
-        except TypeError:  # Python 3.10
-            cls = _process_class(
-                cls,
-                init=False,
-                repr=repr,
-                eq=eq,
-                order=order,
-                unsafe_hash=unsafe_hash,
-                frozen=frozen,
-                match_args=match_args,
-                kw_only=kw_only,
-                slots=slots,
-            )
 
-        fields = cls.__dict__["__dataclass_fields__"]
+        def inner_dataclass(inner_cls, init):
+            if sys.version_info >= (3, 11):
+                return _dataclass(
+                    init=init,
+                    repr=repr,
+                    eq=eq,
+                    order=order,
+                    unsafe_hash=unsafe_hash,
+                    frozen=frozen,
+                    match_args=match_args,
+                    kw_only=kw_only,
+                    slots=slots,
+                    weakref_slot=weakref_slot,
+                )(inner_cls)
+            else:
+                return _dataclass(
+                    init=init,
+                    repr=repr,
+                    eq=eq,
+                    order=order,
+                    unsafe_hash=unsafe_hash,
+                    frozen=frozen,
+                    match_args=match_args,
+                    kw_only=kw_only,
+                    slots=slots,
+                )(inner_cls)
 
-        def gen_fields():
-            """
-            Generates dataclass fields with _MISSING default value when shadowing abstract properties.
+        # create dataclass without init method to avoid non-default argument 
+        # follows default argument error
+        decorated_cls = inner_dataclass(cls, init=False)
 
-            Abstract properties that shadow dataclass fields are added as default values to those fields. If this default
-            value (a property object) is not removed, the dataclass will incorrectly assign the property object as the
-            field's value when an argument is not provided during initialization.
-
-            Example:
-
-            ```python
-            from abc import ABC, abstractmethod
-            from dataclassabc import dataclassabc
-
-            class A(ABC):
-                @property
-                @abstractmethod
-                def name(self) -> str: ...
-
-            @dataclassabc(frozen=True)
-            class B(A):
-                name: str
-
-            # Attempting to create an instance without providing a value for 'name'
-            b = B()
-            print(b)   # Output: B(name=<property object at ...>)
-            ```
-
-            Once the field's default value is remove, initializing the dataclass `B` without providing a value for the `name` argument
-            will result in an error.
-
-            ``` python
-            # TypeError: B.__init__() missing 1 required positional argument: 'name'
-            b = B()
-            ```
-            """
-
+        if init:
+            # remove abstract properties from default field values 
+            fields = decorated_cls.__dataclass_fields__
             for field in fields.values():
                 if field._field_type in (_FIELD, _FIELD_INITVAR):
                     if (
@@ -261,44 +225,19 @@ def dataclassabc(
                     ):
                         field.default = _MISSING
 
-                    yield field
-
-        all_init_fields = list(gen_fields())
-
-        if cls.__module__ in sys.modules:
-            globals = sys.modules[cls.__module__].__dict__
-        else:
-            globals = {}
-
-        (std_init_fields, kw_only_init_fields) = _fields_in_init_order(all_init_fields)
-
-        has_post_init = hasattr(cls, _POST_INIT_NAME)
-
-        # Recreate the __init__ method after removing default values from the fields
-        _set_new_attribute(
-            cls,
-            "__init__",
-            _init_fn(
-                all_init_fields,
-                std_init_fields,
-                kw_only_init_fields,
-                frozen,
-                has_post_init,
-                # The name to use for the "self"
-                # param in __init__.  Use "self"
-                # if possible.
-                "__dataclass_self__" if "self" in fields else "self",
-                globals,
-                slots,
-            ),
-        )
+            cls_no_init = type(
+                cls.__name__,
+                (decorated_cls,),
+                {},
+            )
+            decorated_cls = inner_dataclass(cls_no_init, init=init)
 
         if slots:
-            return cls
-        
-        else:
-            # Create a property for each abstract property that is overridden by a corresponding dataclass field
-            return resolve_abc_prop(cls)
+            return decorated_cls
+
+        # Create a property for each abstract property that is implemented by a 
+        # dataclass field
+        return resolve_abc_prop(decorated_cls)
 
     if _cls is None:
         return wrap
